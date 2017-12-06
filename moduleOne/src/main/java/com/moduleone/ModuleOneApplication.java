60623b7eeb43c33c07115e4748747735d381cfab
George <george.john.rosario@gmail.com>
2017-12-06 17:52:00 +0530

    InitialCommit

---
+++ moduleOne/src/main/java/com/moduleone/ModuleOneApplication.java
@@ -0,0 +1,21 @@
+package com.moduleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ModuleOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleOneApplication.class, args);
+	}
+	
+	@RequestMapping(value="/greeting", method=RequestMethod.GET)
+	public String hello() {
+		return "Hello There, Welcome to MicroService ";
+	}
+}
